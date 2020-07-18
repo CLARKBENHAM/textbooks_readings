87e2152ad328b4f33a8274c643f28325fe04873e
CLARKBENHAM <clark.benham@gmail.com>
2020-07-18 14:37:20 -0400

    init

---
+++ Option Pricing And Volatility - Advanced Strategies And Trading Techniques by Sheldon Natenberg/todo.py
@@ -0,0 +1,5 @@
+#automatic check for option Arbs, see
+#file:///C:/Users/student.DESKTOP-UT02KBN/Dropbox/Readings/Advanced%20Finance/Option%20Pricing%20And%20Volatility%20-%20Advanced%20Strategies%20And%20Trading%20Techniques%20-%20Sheldon%20Natenberg%20-%201994.pdf#page=242
+#or better
+#file:///C:/Users/student.DESKTOP-UT02KBN/Dropbox/Readings/Advanced%20Finance/Option%20Pricing%20And%20Volatility%20-%20Advanced%20Strategies%20And%20Trading%20Techniques%20-%20Sheldon%20Natenberg%20-%201994.pdf#page=462
+
